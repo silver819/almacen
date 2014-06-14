2fb0ad9341a9027335e47608a539fccbba0f2015
Cristina Sanchez <silver819@gmail.com>
2014-06-14 12:18:09 +0200

    actualizo

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new UserNew\UserBundle\UserNewUserBundle(),
             new Reservable\ActivityBundle\ReservableActivityBundle(),
+            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
